de407b56544802a8c769bbbd8f9c0917e7e19c69
Mahaveer Rulaniya <65475928+mahaveer-rulaniya@users.noreply.github.com>
2021-02-02 19:10:20 +0530

    day 1

---
+++ calc.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a;
+  int b;
+  
+  cout<< "Type the first number\n";
+  cin>> a;
+
+  cout<< "Type the second number\n";
+  cin>> b;
+
+  int sum = a+b;
+  cout<< "The sum of both numbers is::";
+  cout<< sum <<endl;
+
+  return 0;
+
+}
